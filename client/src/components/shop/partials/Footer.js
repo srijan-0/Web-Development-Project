c8359d49294f49fa648f042ba58ee8d10a0f834c
srijan-0 <srijan.shrestha000@gmail.com>
2025-02-16 15:14:33 +0545

    Fix UI responsiveness issues

---
+++ client/src/components/shop/partials/Footer.js
@@ -0,0 +1,17 @@
+import moment from "moment";
+import React, { Fragment } from "react";
+
+const Footer = (props) => {
+  return (
+    <Fragment>
+      <footer
+        style={{ background: "#303031", color: "#87898A" }}
+        className="z-10 py-6 px-4 md:px-12 text-center"
+      >
+        Develop by Srijan {moment().format("YYYY")}
+      </footer>
+    </Fragment>
+  );
+};
+
+export default Footer;
